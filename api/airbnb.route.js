9899cc3b272a7c8face7e6144f30725e8afd6b66
unknown <shutel.miro@gmail.com>
2021-07-28 22:12:46 +0300

    added add/update/delete actions and review and model controller

--- api/airbnb.route.js
+++ api/airbnb.route.js
@@ -1,8 +1,9 @@
 import express from "express"
 import AirBnbController from "./airbnb.controller.js";
+import ReviewsController from "./reviews.controller.js";
 
 const router = express.Router();
 router.route("/").get(AirBnbController.apiGetAirBnb);
-//router.route("review").post(AirBnbController.apiPostReview).put(AirBnbController.apiUpdateReview).delete(AirBnbController.apiDeleteReview)
+router.route("/review").post(ReviewsController.apiPostReview).put(ReviewsController.apiUpdateReview).delete(ReviewsController.apiDeleteReview);
 
 export default router;
--- index.js
+++ index.js
@@ -2,23 +2,23 @@ import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import AirbnbModel from "./Data/AirbnbModel.js"
+import ReviewsModel from "./Data/ReviewsModel.js"
 
 dotenv.config();
 const MongoClint = mongodb.MongoClient;
 
 const port = process.env.PORT || 8000;
 
-MongoClint.connect(process.env.AIRBNB_DB_URI,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500,
-    }).catch(err => {
-        console.error(err.stack);
-        process.exit(1);
-    }).then(async client => {
-        await AirbnbModel.injectDB(client);
-
-        app.listen(port, () => {
-            console.log(`listening on port ${port}`);
-        })
+MongoClint.connect(process.env.AIRBNB_DB_URI, {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+}).catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+}).then(async client => {
+    await AirbnbModel.injectDB(client);
+    await ReviewsModel.injectDB(client);
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`);
     })
+})
